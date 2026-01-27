a46976c980248d978d923ebe4f8ed176ff5eb405
Gyanendra Bundela <gyanendra.bundela@centricconsulting.com>
2026-01-28 03:15:41 +0530

    Refactor driver handling using ThreadLocal DriverManager for parallel Appium execution

--- src/main/java/driver/DriverFactory.java
+++ src/main/java/driver/DriverFactory.java
@@ -5,6 +5,8 @@ import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DriverFactory {
 
@@ -15,12 +17,9 @@ public class DriverFactory {
         URL serverUrl;
 
         if (runMode.equalsIgnoreCase("cloud")) {
-
             String user = System.getenv("BROWSERSTACK_USER");
             String key = System.getenv("BROWSERSTACK_KEY");
-
             serverUrl = new URL("https://" + user + ":" + key + "@hub-cloud.browserstack.com/wd/hub");
-
         } else {
             serverUrl = new URL("http://127.0.0.1:" + port);
         }
@@ -29,15 +28,31 @@ public class DriverFactory {
 
             UiAutomator2Options options = new UiAutomator2Options();
 
-            options.setDeviceName(ConfigManager.get("deviceName"));
-            options.setPlatformName("Android");
-            options.setPlatformVersion(ConfigManager.get("platformVersion"));
-            options.setApp(System.getenv("BROWSERSTACK_APP"));
-            options.setAutomationName("UiAutomator2");
+            if (runMode.equalsIgnoreCase("cloud")) {
 
-            options.setCapability("project", "TimeTo Mobile");
-            options.setCapability("build", "GitHub CI");
-            options.setCapability("name", "Android Smoke Test");
+                Map<String, Object> bstackOptions = new HashMap<>();
+                bstackOptions.put("userName", System.getenv("BROWSERSTACK_USER"));
+                bstackOptions.put("accessKey", System.getenv("BROWSERSTACK_KEY"));
+                bstackOptions.put("deviceName", ConfigManager.get("deviceName"));
+                bstackOptions.put("osVersion", ConfigManager.get("platformVersion"));
+                bstackOptions.put("projectName", "TimeTo Mobile");
+                bstackOptions.put("buildName", "GitHub CI");
+                bstackOptions.put("sessionName", "Android Smoke");
+
+                options.setCapability("bstack:options", bstackOptions);
+                options.setCapability("app", System.getenv("BROWSERSTACK_APP"));
+                options.setCapability("platformName", "Android");
+
+            } else {
+                options.setDeviceName(ConfigManager.get("deviceName"));
+                options.setPlatformName("Android");
+                options.setPlatformVersion(ConfigManager.get("platformVersion"));
+                options.setAppPackage(ConfigManager.get("appPackage"));
+                options.setAppActivity(ConfigManager.get("appActivity"));
+                options.setApp(ConfigManager.get("appPath"));
+                options.autoGrantPermissions();
+                options.setSystemPort(0);
+            }
 
             return new AppiumDriver(serverUrl, options);
         }
